73cdb59201d67dfefff59929ddd3cddba3c33ea1
amal2000-cse <kannothparambathamalmanoj@gmail.com>
2024-01-28 17:53:58 +0530

    updated link

--- client/src/pages/CreatePostt.jsx
+++ client/src/pages/CreatePostt.jsx
@@ -19,7 +19,7 @@ const CreatePostt = () => {
     if(form.prompt){
       try {
         setGeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle',{
+        const response = await fetch('https://dall-e-3d8u.onrender.com/api/v1/dalle',{
           //we are providing options over here
           method:'POST',
           headers:{
@@ -51,7 +51,7 @@ const CreatePostt = () => {
     if(form.prompt && form.photo){
       setLoading(true);
       try {
-        const response=await fetch('http://localhost:8080/api/v1/post',{
+        const response=await fetch('https://dall-e-3d8u.onrender.com/api/v1/post',{
           method:'POST',
           headers:{
             'Content-Type':'application/json',
